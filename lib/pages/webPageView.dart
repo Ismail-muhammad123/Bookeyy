ea033a4e0219586ddb263010166a9daf5baadd97
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2020-09-28 16:22:17 -0700

    change the lunch icons of the android platform and the webview widget to a futurebuilder.

--- lib/pages/webPageView.dart
+++ lib/pages/webPageView.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'package:wp_json_api/wp_json_api.dart';
+//import 'package:wp_json_api/wp_json_api.dart';
 
 class WebPageView extends StatefulWidget {
   WebPageView({this.url});
@@ -17,9 +18,18 @@ class _WebPageViewState extends State<WebPageView> {
   @override
   void initState() {
     super.initState();
-    WPJsonAPI.instance.initWith(baseUrl: "https://bookeyy.com");
+
     isloading = true;
   }
+//  Completer<WebViewController> _controller = Completer<WebViewController>();
+//
+//  checkInternetConectivity() async {
+//    bool result = await DataConnectionChecker().hasConnection;
+//    if (result == true) {
+//      return true;
+//    }
+//    return false;
+//  }
 
   bool isloading;
   WebViewController _webViewcontroller;
@@ -27,102 +37,92 @@ class _WebPageViewState extends State<WebPageView> {
 
   @override
   Widget build(BuildContext context) {
-    try {
-      http
-          .get("https://www.google.com")
-          .then(
-            (value) => setState(
-              () {
-                withInternet = true;
-              },
-            ),
-          )
-          .catchError(
-        (err) {
-          setState(
-            () {
-              withInternet = false;
-            },
-          );
-        },
-      );
-    } catch (e) {
-      print(e);
-    }
+    return FutureBuilder(
+      future: DataConnectionChecker().hasConnection,
+      builder: (context, snapshot) {
+        if (snapshot.hasData ) {
+//          setState(() {
+//            withInternet = true;
+//          });
+          return Stack(
+            children: [
+              Scaffold(
+                body: SafeArea(
+                  child: WillPopScope(
+                    onWillPop: () async {
+                      var canGoBack = _webViewcontroller.canGoBack();
 
-    if (withInternet == false) {
-      return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text(
-                  "Sorry, an error has occurs. check your internet and try again",
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                ),
-              ),
-              MaterialButton(
-                color: Colors.blue,
-                child: Text("Retry"),
-                onPressed: () => setState(() {
-                  withInternet = true;
-                }),
-              )
-            ],
-          ),
-        ),
-      );
-    }
-    return Stack(
-      children: [
-        Scaffold(
-          body: SafeArea(
-            child: WillPopScope(
-              onWillPop: () async {
-                var canGoBack = _webViewcontroller.canGoBack();
-
-                if (canGoBack as bool) {
-                  _webViewcontroller.goBack();
-                  return false;
-                } else {
-                  return true;
-                }
-              },
-              child: WebView(
-                initialUrl: widget.url,
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (controller) {
-                  setState(
-                    () {
-                      _webViewcontroller = controller;
+                      if (canGoBack as bool) {
+                        _webViewcontroller.goBack();
+                        return false;
+                      } else {
+                        return true;
+                      }
                     },
-                  );
+                    child: WebView(
+                      initialUrl: widget.url,
+                      javascriptMode: JavascriptMode.unrestricted,
+                      onWebViewCreated: (controller) {
+                        setState(
+                          () {
+                            _webViewcontroller = controller;
+                          },
+                        );
 
-                  Timer(
-                    Duration(seconds: 5),
-                    () => setState(
-                      () {
-                        isloading = false;
+                        Timer(
+                          Duration(seconds: 5),
+                          () => setState(
+                            () {
+                              isloading = false;
+                            },
+                          ),
+                        );
                       },
                     ),
-                  );
-                },
+                  ),
+                ),
+              ),
+              isloading
+                  ? Center(
+                      child: CircularProgressIndicator(
+                      backgroundColor: Colors.blue,
+                    ))
+                  : Center()
+            ],
+          );
+        } else {
+//          setState(() {
+//            withInternet = false;
+//          });
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      "Sorry, an error has occurs. check your internet and try again",
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.w500),
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                    ),
+                  ),
+                  MaterialButton(
+                    color: Colors.blue,
+                    child: Text("Retry"),
+                    onPressed: () => setState(() {
+                      withInternet = true;
+                    }),
+                  )
+                ],
               ),
             ),
-          ),
-        ),
-        isloading
-            ? Center(
-                child: CircularProgressIndicator(
-                backgroundColor: Colors.blue,
-              ))
-            : Center()
-      ],
+          );
+        }
+      },
     );
   }
 }
